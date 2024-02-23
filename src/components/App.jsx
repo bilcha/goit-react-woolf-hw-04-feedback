c4754f74f893988bfb759394c751b9c0efc35e3f
Olena <33802160+bilcha@users.noreply.github.com>
2024-02-23 11:53:10 -0800

    fix code according to the comments

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Statistics } from './Statistics/Statistics';
 import { Section } from './Section/Section';
@@ -8,16 +8,8 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  const [totalCount, setTotalCount] = useState(0);
-  const [positivePercentage, setPositivePercentage] = useState(0);
 
-  useEffect(() => {
-    const total = good + neutral + bad;
-    setTotalCount(total);
-    setPositivePercentage(((good / total) * 100).toFixed());
-  }, [good, bad, neutral]);
-
-  function onLeaveFeedback(val) {
+  const onLeaveFeedback = val => {
     if (val === 'good') {
       setGood(prev => prev + 1);
     } else if (val === 'neutral') {
@@ -25,7 +17,13 @@ const App = () => {
     } else if (val === 'bad') {
       setBad(prev => prev + 1);
     }
-  }
+  };
+  const countTotalFeedback = () => {
+    return good + neutral + bad;
+  };
+  const countPositiveFeedbackPercentage = () => {
+    return ((good / countTotalFeedback()) * 100).toFixed();
+  };
   return (
     <div className="wrapper">
       <Section title="Please leave feedback">
@@ -36,15 +34,15 @@ const App = () => {
       </Section>
 
       <Section title="Statistics">
-        {totalCount === 0 ? (
+        {countTotalFeedback() === 0 ? (
           <Notification message="There is no feedback" />
         ) : (
           <Statistics
             good={good}
             neutral={neutral}
             bad={bad}
-            total={totalCount}
-            positivePercentage={positivePercentage}
+            total={countTotalFeedback()}
+            positivePercentage={countPositiveFeedbackPercentage()}
           />
         )}
       </Section>
